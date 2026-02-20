d723754abec2ea0d5d3686690cb11cac3221926d
ProfMilk <mikeis2salty@gmail.com>
2026-02-20 09:42:02 -0500

    Wrd

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        {/*anytime we wan to add tailwind ccs with our custom css we have to put it inside tick*/}
         <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
